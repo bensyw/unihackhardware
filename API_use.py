a095d49b778971243f28d255ceb15e218050f0de
shiyun <shiyun.zhangsyz@gmail.com>
2017-08-19 18:23:07 +1000

    update upload image function

--- API_use.py
+++ API_use.py
@@ -3,30 +3,32 @@
 import requests
 import base64
 from requests.auth import HTTPBasicAuth
+import json
 
-with open("aussie.jpg", "rb") as image_file:
-    encoded_string = base64.b64encode(image_file.read())
-    # print(encoded_string)
 
-headers = {
+user = 'demo1'
+password = 'hackathon7493'
+json_headers = {
     'Content-Type': 'application/json',
 }
 
-
-data = '{"service":"tagging1","image":"' + encoded_string.decode() + """ "}"""
 # print(data)
 
 # post_call = requests.post('http://smartvision.aiam-dh.com:8080/api/v1.0/tasks',
 #                           headers=headers, data=data, auth=HTTPBasicAuth('demo1', 'hackathon7493'))
 
 
-def PostImage():
-    post_call = requests.post('http://smartvision.aiam-dh.com:8080/api/v1.0/tasks',
-                              headers=headers, data=data, auth=HTTPBasicAuth('demo1', 'hackathon7493'))
-    print(post_call, "POST call")
-    print(post_call.text, "TEXT")
-    print(post_call.content, "CONTENT")
-    print(post_call.status_code, "STATUS CODE")
+# input is the path to the image
+def upload_image(image_path):
+    with open("aussie.jpg", "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+        data_send = '{"service":"tagging1","image":"' + encoded_string.decode() + """ "}"""
+        post_call = requests.post('http://smartvision.aiam-dh.com:8080/api/v1.0/tasks', headers=json_headers, data=data_send, auth=HTTPBasicAuth(user, password))
+        # print out debug info
+        # print(post_call, "POST call")
+        # print(post_call.text, "TEXT")
+        # print(post_call.content, "CONTENT")
+        # print(post_call.status_code, "STATUS CODE")
 
 
 def GetInfoImage():
@@ -35,9 +37,9 @@ def GetInfoImage():
 
 
 def GetInfoAll():
-    get_call = requests.get('http://smartvision.aiam-dh.com:8080/api/v1.0/tasks',
-                            headers=headers, auth=HTTPBasicAuth('demo1', 'hackathon7493'))
-
+    get_call = requests.get('http://smartvision.aiam-dh.com:8080/api/v1.0/tasks', headers=json_headers, auth=HTTPBasicAuth(user, password))
+    print(get_call.text, "TEXT")
+    return json.loads(get_call.text)
 
 def PutUpdate():
     pass
@@ -48,4 +50,11 @@ def PutRun():
 
 
 def Delete():
+    import requests
+
+requests.delete('http://smartvision.aiam-dh.com:8080/api/v1.0/tasks/123', headers=headers, auth=('usr', 'pwd'))
     pass
+
+if __name__ == "__main__":
+    data = GetInfoAll()
+    print(data)
