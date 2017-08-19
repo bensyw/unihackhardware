e9a21b3bf397a2631be9c59b19f79f44d4dbf6f3
Shengyu Wang <benjamin@ShengyudeMacBook-Air.local>
2017-08-19 17:05:20 +1000

    HTTP Request

---
+++ API_use.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import requests
+import base64
+from requests.auth import HTTPBasicAuth
+
+with open("aussie.jpg", "rb") as image_file:
+	encoded_string = base64.b64encode(image_file.read())
+	#print(encoded_string)
+
+headers = {
+    'Content-Type': 'application/json',
+}
+
+
+data = '{"service":"tagging1","image":"' + encoded_string.decode() + """ "}"""
+print(data)
+
+post_call = requests.post('http://smartvision.aiam-dh.com:8080/api/v1.0/tasks', headers=headers, data=data, auth=HTTPBasicAuth('demo1', 'hackathon7493'))
+
+
+print(post_call, "POST call")
+print(post_call.text, "TEXT")
+print(post_call.content, "CONTENT")
+print(post_call.status_code, "STATUS CODE")
