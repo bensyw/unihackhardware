01dfc51dc1fcd26579711046066dd61d0af10b82
Shengyu Wang <benjamin@ShengyudeMacBook-Air.local>
2017-08-19 17:26:53 +1000

    Move post_call to a function

--- API_use.py
+++ API_use.py
@@ -16,18 +16,17 @@ headers = {
 data = '{"service":"tagging1","image":"' + encoded_string.decode() + """ "}"""
 # print(data)
 
-post_call = requests.post('http://smartvision.aiam-dh.com:8080/api/v1.0/tasks',
-                          headers=headers, data=data, auth=HTTPBasicAuth('demo1', 'hackathon7493'))
+# post_call = requests.post('http://smartvision.aiam-dh.com:8080/api/v1.0/tasks',
+#                           headers=headers, data=data, auth=HTTPBasicAuth('demo1', 'hackathon7493'))
 
-
-# print(post_call, "POST call")
-# print(post_call.text, "TEXT")
-# print(post_call.content, "CONTENT")
-# print(post_call.status_code, "STATUS CODE")
 
 def PostImage():
     post_call = requests.post('http://smartvision.aiam-dh.com:8080/api/v1.0/tasks',
                               headers=headers, data=data, auth=HTTPBasicAuth('demo1', 'hackathon7493'))
+    print(post_call, "POST call")
+    print(post_call.text, "TEXT")
+    print(post_call.content, "CONTENT")
+    print(post_call.status_code, "STATUS CODE")
 
 
 def GetInfoImage():
@@ -35,8 +34,13 @@ def GetInfoImage():
 
 
 def GetInfoAll():
-    get_call = requests.get('http://smartvision.aiam-dh.com:8080/api/tasks',
-                            headers=headers, auth=HTTPBasicAuth('demo1', 'hackathon7493'))
+    pass
+    # get_call = requests.get('http://smartvision.aiam-dh.com:8080/api/tasks',
+    #                         headers=headers, auth=HTTPBasicAuth('demo1', 'hackathon7493'))
+    # print(get_call, "POST call")
+    # print(get_call.text, "TEXT")
+    # print(get_call.content, "CONTENT")
+    # print(get_call.status_code, "STATUS CODE")
 
 
 def PutUpdate():
